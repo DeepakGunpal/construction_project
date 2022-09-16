d1ef1c5ac2f4ab2bcd3a40657ebe7a2c299e16f8
Deepak <deepakgunpal0@gmail.com>
2022-09-16 19:47:24 +0530

    completed

--- src/controller/project.js
+++ src/controller/project.js
@@ -96,7 +96,7 @@ const createPDF = async (req, res) => {
         res.status(200).send({ data: details });
     } catch (error) {
         console.log(error);
-        res.status(500).send({ message: { e: error.message, e: error } });
+        res.status(500).send({ message: error.message });
     }
 }
 

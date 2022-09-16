973d0a9235b43782ce13cacf08bb7dd544f3927f
Deepak <deepakgunpal0@gmail.com>
2022-09-16 07:45:40 +0530

    static file changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Home from './pages/home/Home';
 function App() {
   return (
     <div className="App">
+      <Home />
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route path='/dashboard' element={<Dashboard />} />

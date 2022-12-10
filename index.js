db6e28ea11521224d61a710f6213ac7ef683366e
Deepak <deepakgunpal0@gmail.com>
2022-12-10 16:35:33 +0530

    construction backend

--- index.js
+++ index.js
@@ -4,23 +4,16 @@ const route = require('./src/route/route.js');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const path = require('path');
 
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', route);
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV == "production") {
-
-    app.use(express.static("client/build"));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
 
 mongoose.connect(process.env.MONGODB, {
     useNewUrlParser: true
